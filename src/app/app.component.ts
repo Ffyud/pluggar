93daaf26da581ec46561ecfcf64c7ae7a3f94d79
David <5263778+Ffyud@users.noreply.github.com>
2024-08-28 12:42:15 +0200

    add some styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from "./card/card.component";
+import { CardReplyComponent } from "./card-reply/card-reply.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardComponent, CardReplyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pluggar';
+  title = 'Pluggar';
 }
